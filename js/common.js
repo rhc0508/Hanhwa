5542739d70689687b7a81702eaa09e0e9071a2d2
rhc0508 <rhc0508@naver.com>
2022-02-21 06:11:10 +0900

    eighth_commit

--- js/common.js
+++ js/common.js
@@ -2,58 +2,69 @@
 $(document).ready(function(){
     $("header").load("./header.html", function(){
         // 이벤트 영역을 넣는다.
+        function resizeEvt(){
+            const $curWidth = $(window).width();
+            console.log($curWidth);
+            console.log("로딩되자마자 리사이즈이벤트");
 
-        // #header_1 메뉴 hover시 서브메뉴 보이기
-        $("#header_1 .wrap nav > ul > li").hover(function(){
-            console.log(this);
-            $(this).find("ul").stop().show();
-        }, function(){
-            $(this).find("ul").stop().hide();
+            if($curWidth < 768+"px"){
+                // max-width:768px에서 버튼 클릭시 서브메뉴 등장
+                const plusBtn = $("#header_2 nav > ul > li > a > span.plusBtn");
+
+                plusBtn.on("click", function(){
+                    console.log("클릭 이벤트!");
+                    const $show = $(this).hasClass("show");
+
+                    if(!$show){
+                        $("#header_2 .wrap nav > ul > li > a > .plusBtn").removeClass("show");
+                        $(this).addClass("show");
+                        $("#header_2 .wrap nav > ul > li > ul").hide();
+                        $(this).closest("li").find("ul").show();
+                    }else{
+                        $(this).removeClass("show");
+                        $(this).closest("li").find("ul").hide();
+                    }
+                    return false;
+                });
+            }else{
+                // #header_1 메뉴 hover시 서브메뉴 보이기
+                $("#header_1 .wrap nav > ul > li").hover(function(){
+                    console.log(this);
+                    $(this).find("ul").stop().show();
+                }, function(){
+                    $(this).find("ul").stop().hide();
+                });
+
+
+                // 햄버거 메뉴바 클릭시 #header_2 열림
+                const $menuBar = $("#header_1 .wrap .right .menuBar");
+                const $closeBtn = $("#header_2 .close a");
+                const $header_2 = $("#header_2");
+
+                $menuBar.on("click", function(){
+                    const $active = $(this).hasClass("active");
+                    if(!$active){
+                        $(this).addClass("active");
+                        $header_2.addClass("active");
+                    }else{
+                        // console.log("닫기");
+                        $(this).removeClass("active");
+                        $header_2.removeClass("active");
+                    }
+                    return false;
+                });
+                $closeBtn.on("click", function(){
+                    $header_2.removeClass("active");
+                    return false;
+                });
+            }
+        }
+        resizeEvt();
+        $(window).resize(function(){
+            resizeEvt();
         });
 
         
-        // 햄버거 메뉴바 클릭시 #header_2 열림
-        const $menuBar = $("#header_1 .wrap .right .menuBar");
-        const $closeBtn = $("#header_2 .close a");
-        const $header_2 = $("#header_2");
-
-        $menuBar.on("click", function(){
-            const $active = $(this).hasClass("active");
-            if(!$active){
-                $(this).addClass("active");
-                $header_2.addClass("active");
-            }else{
-                // console.log("닫기");
-                $(this).removeClass("active");
-                $header_2.removeClass("active");
-            }
-            return false;
-        });
-        $closeBtn.on("click", function(){
-            $header_2.removeClass("active");
-            return false;
-        });
- 
-        // min-width:767px에서 + 버튼 클릭시 서브메뉴 등장
-        const plusBtn = $("#header_2 nav > ul > li > a > span.plusBtn");
-
-        plusBtn.on("click", function(){
-            console.log("클릭 이벤트!");
-            const $show = $(this).hasClass("show");
-
-            if(!$show){
-                $("#header_2 .wrap nav > ul > li > a > .plusBtn").removeClass("show");
-                $(this).addClass("show");
-                $("#header_2 .wrap nav > ul > li > ul").hide();
-                $(this).closest("li").find("ul").show();
-            }else{
-                $(this).removeClass("show");
-                $(this).closest("li").find("ul").hide();
-            }
-            return false;
-        });
-
-    
 
     });
 
